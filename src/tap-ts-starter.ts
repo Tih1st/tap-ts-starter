46d44f9d8f7e601460beaf4e22b0067bdf38b795
Jay <jijunsui@ymail.com>
2018-05-24 17:06:30 -0700

    yaml files
    yaml parser

--- src/tap-ts-starter.ts
+++ src/tap-ts-starter.ts
@@ -3,27 +3,28 @@
 // ...
 export default class DummyClass {}
 
-console.log(
-  'working!'
-) /** hack for https://github.com/TypeStrong/typedoc/issues/603 */
+const anyjson = require('any-json')
+console.log('working!') /** hack for https://github.com/TypeStrong/typedoc/issues/603 */
 
 /**
  * This module is the entry point for local execution as a Singer tap (see the [spec](https://github.com/singer-io/getting-started/blob/master/SPEC.md))
  */
+
+import * as parseYaml from './parse-yaml'
 import * as configLoader from './tap-load-config'
-import * as parseMime from './parse-mime'
+//import * as parseMime from './parse-mime'
 import * as scanDir from './scan-dir'
 
 /** random note */
 
 /** random note 2
-  * 
-  */
+ *
+ */
 configLoader
   .loadConfig()
   .then(function(configObjs) {
     // run scanDir using parseMime as the parser for each item
-    return scanDir.scanDir(configObjs, parseMime.parseItem)
+    return scanDir.scanDir(configObjs, parseYaml.parseItem)
   })
   .catch(function(error) {
     // Handle errors
